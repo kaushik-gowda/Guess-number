95f7756ddab39145820e5cf99937e464f1fe9085
KAUSHIK P <1nt23ad025.kaushik@nmit.ac.in>
2025-02-12 23:42:06 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,13 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("please type a number larger than 0.")
+        quit()
+else:
+    print("please type a number next time.")
+    quit()
