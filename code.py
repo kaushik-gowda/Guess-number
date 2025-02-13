f12df6d19152f602a6c1719142bc3550becf11c9
KAUSHIK P <1nt23ad025.kaushik@nmit.ac.in>
2025-02-13 09:55:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -11,3 +11,14 @@ if top_of_range.isdigit():
 else:
     print("please type a number next time.")
     quit()
+    
+random_number = random.randint(0, top_of_range)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("please type a number next time.")
+        continue
